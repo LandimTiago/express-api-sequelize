baec667540bf42d184e21379694cdcafd2bf5aad
TortoiseTiago <tortoise.tl@gmail.com>
2020-05-22 13:43:15 -0300

    create base controller

--- src/app/book/controller.js
+++ src/app/book/controller.js
@@ -1,39 +1,6 @@
-const { Response } = require('../../libs/response');
+const { BaseController } = require('../../base');
 const { BookService } = require('./service');
 
-// async function create(req, res) {
-//   return response.send(res, await Book.create(req.body));
-// }
-// async function updateById(req, res) {
-//   return response.send(res, await Book.updateById(req.params.id, req.body));
-// }
-// async function deleteById(req, res) {
-//   return response.send(res, await Book.deleteById(req.params.id));
-// }
-// async function findAll(req, res) {
-//   return response.send(res, await Book.findAll());
-// }
-// async function findById(req, res) {
-//   return response.send(res, await Book.findById(req.params.id));
-// }
+const BookController = BaseController.init(BookService);
 
-const create = async (req, res) =>
-  Response.send(res, await BookService.create(req.body));
-const updateById = async (req, res) =>
-  Response.send(res, await BookService.updateById(req.params.id, req.body));
-const deleteById = async (req, res) =>
-  Response.send(res, await BookService.deleteById(req.params.id));
-const findAll = async (req, res) =>
-  Response.send(res, await BookService.findAll());
-const findById = async (req, res) =>
-  Response.send(res, await BookService.findById(req.params.id));
-
-module.exports = {
-  BookController: {
-    create,
-    updateById,
-    deleteById,
-    findById,
-    findAll,
-  },
-};
+module.exports = { BookController };
